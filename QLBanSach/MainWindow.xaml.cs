4997dd173c02847f2fdedcca35ec6846497c4770
Trần Trung Hậu <trunghau.t1@gmail.com>
2015-11-16 00:22:48 +0700

    Update

--- QLBanSach/MainWindow.xaml.cs
+++ QLBanSach/MainWindow.xaml.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FlatTheme.ControlStyle;
+using QLBanSach.View;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace QLBanSach
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : FlatWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            //MainGrid.Children.Add(new DangNhap());
         }
     }
 }
